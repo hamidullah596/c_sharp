e3be4e9383802d0c5353ab841af7315f47c0c3ac
Hamidullah Amini <133311487+hamidullah596@users.noreply.github.com>
2023-05-28 22:13:55 -0700

    week7

---
+++ week6.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[4] { 4, 6, 8, 10, };
+            int j = 0;
+          
+            foreach (int i in a)
+            {
+                j = i + 2;
+            }
+          
+            foreach(int i in a)
+            {
+                Console.Write(i +"\t");
+            }
+            Console.ReadLine();
+        }
+    }
+}
