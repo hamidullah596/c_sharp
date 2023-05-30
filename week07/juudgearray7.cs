438a3ec3bccfa015b50aee808f52836be9bb0b7f
Hamidullah Amini <133311487+hamidullah596@users.noreply.github.com>
2023-05-29 21:51:10 -0700

    Add files via upload

---
+++ week07/juudgearray7.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuggedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[][] a = new int[3][];
+            a[0] = new int[4];
+            a[1] = new int[3];
+            a[2] = new int[2];
+          
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Enter the numbers: ");
+                for (int j = 0; j < a[i].GetLength(0); j++)
+                {
+                    a[i][j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+           
+            int k = 0;
+            foreach (int[] x in a)
+            {
+                Console.Write($"a[{k}] = ");
+                foreach (int y in x)
+                {
+                    Console.Write(y + "\t");
+                }
+                k++;
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week07/multidimotionalarray7.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiDoimentiondArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] mat1 = new int[3, 4];
+            int[,] mat2 = new int[4, 2] { { 2, 3 }, { 4, 5 }, { 1, 7 }, { 0, 9 } };
+            int[,] mat3 = {{1, 2, 3 },{1, 2, 3},{1, 2, 3} };
+
+
+
+            Console.WriteLine("Your numbers are in mat3: ");
+            int counter = 0;
+            foreach(int i in mat3)
+            {
+                Console.Write(i + "\t");
+                counter++;
+                if (counter == 3)
+                {
+                    counter = 0;
+                    Console.WriteLine("\t");
+                }
+            }
+            Console.WriteLine();
+
+            for (int j = 0; j < 3; j++)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write(mat3[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
